295d20fa8de42150acfe5282bebf45bd9065603e
BCEvanFang <evanfang@moneysq.com>
2019-10-09 10:40:32 +0800

    Version 2 refactor: 1. Class rename 2. Method extraction

--- PrimeService/GeneratePrimes.cs
+++ PrimeService/GeneratePrimes.cs
@@ -1,7 +1,11 @@
 ﻿using System;
 
-public class GeneratePrimes
+public class PrimeGenerator
 {
+  private static int s;
+  private static bool[] f;
+  private static int[] primes;
+
   ///<summary>
   ///產生一個包含質數的陣列
   ///</summary>
@@ -9,51 +13,69 @@ public class GeneratePrimes
   /// <param name="maxValue">產生的最大值</param>
   public static int[] GeneratePrimeNumbers(int maxValue)
   {
-    if (maxValue >= 2) // 僅在此時有意義
+    if (maxValue < 2)
     {
-      // 宣告
-      int s = maxValue + 1; // 陣列大小
-      bool[] f = new bool[s];
-      int i;
-
-      // 將陣列元素初始化為true.
-      for (i = 0; i < s; i++)
-        f[i] = true;
-
-      // 去掉已知的非質數
-      f[0] = f[1] = false;
-
-      // sieve(篩選；過濾)
-      int j;
-      for (i = 2; i < Math.Sqrt(s) + 1; i++)
-      {
-        if (f[i])
-        {
-          for (j = 2 * i; j < s; j += i)
-            f[j] = false;
-        }
-      }
-
-      // 有多少個質數?
-      int count = 0;
-      for (i = 0; i < s; i++)
-      {
-        if (f[i])
-          count++;
-      }
-
-      int[] primes = new int[count];
-
-      // 把質數轉移到結果陣列中
-      for (i = 0, j = 0; i < s; i++)
-      {
-        if (f[i]) // 若為質數
-          primes[j++] = i;
-      }
-
-      return primes; // 回傳primes結果陣列
+      return new int[0];
     }
-    else // maxValue < 2
-      return new int[0]; // 若輸入不合理的值，則回傳空陣列
+    else
+    {
+      InitializeSieve(maxValue);
+      Sieve();
+      LoadPrimes();
+      return primes;
+    }
+  }
+
+  private static void LoadPrimes()
+  {
+    int i;
+    int j;
+
+    // 有多少個質數?
+    int count = 0;
+    for (i = 0; i < s; i++)
+    {
+      if (f[i])
+        count++;
+    }
+
+    primes = new int[count];
+
+    // 把質數轉移到結果陣列中
+    for (i = 0, j = 0; i < s; i++)
+    {
+      if (f[i]) // 質數
+        primes[j++] = i;
+    }
+  }
+
+  private static void Sieve()
+  {
+    int i;
+    int j;
+
+    for (i = 2; i < Math.Sqrt(s) + 1; i++)
+    {
+      if (f[i]) // 如果未被劃掉，就劃掉其倍數
+      {
+        for (j = 2 * i; j < s; j += i)
+          f[j] = false; // 倍數不是質數
+      }
+    }
+  }
+
+  private static void InitializeSieve(int maxValue)
+  {
+    // 宣告
+    s = maxValue + 1; // 陣列大小
+    f = new bool[s];
+    int i;
+
+    // 將陣列元素初始化為true.
+    for (i = 0; i < s; i++)
+      f[i] = true;
+
+    // 去掉已知的非質數
+    f[0] = f[1] = false;
   }
 }
